4e74e587d57ff598efd7f9c86233db98081e1964
peanutsee <dsee007@yahoo.com>
2026-04-20 12:45:09 +0800

    fix: bug fix on check_user_intent middleware

--- src/middleware/middleware.py
+++ src/middleware/middleware.py
@@ -5,7 +5,7 @@ from langchain_openai import ChatOpenAI
 from src.core.prompts.prompts import intent_classification_prompt
 from langchain_core.messages import AIMessage
 
-@before_agent
+@before_agent(can_jump_to=["end"])
 def check_user_intent(state: AgentState, config: dict = None) -> dict | AIMessage | None:
     llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
     structured_llm = llm.with_structured_output(InputValidationResponseFormat)
@@ -15,8 +15,9 @@ def check_user_intent(state: AgentState, config: dict = None) -> dict | AIMessag
     res: InputValidationResponseFormat = structured_llm.invoke(messages)
     
     if not res.is_related:
-        return AIMessage(
-            content=f"I'm sorry, I can only assist with questions regarding portfolios, exposures, or books of clients. {res.reasoning}"
-        )
+        return {
+            "messages": [AIMessage(content=f"I'm sorry, I can only assist with questions regarding portfolios, exposures, or books of clients. {res.reasoning}")],
+            "jump_to": "end"
+        }
     
     return None
